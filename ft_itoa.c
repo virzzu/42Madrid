f516f397dac270aa2d72a14c7238634c0a914d55
Virginia Garcia-Loygorri Arias <vgarcia-@c2r6s3.42madrid.com>
2025-02-06 18:03:03 +0100

    vaya dolor de cabeza

---
+++ ft_itoa.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgarcia- <vgarcia-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 14:44:33 by vgarcia-          #+#    #+#             */
+/*   Updated: 2025/02/06 17:48:47 by vgarcia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_len(int n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int			num;
+	char		*str;
+	size_t		len;
+
+	num = n;
+	len = ft_len(n);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len--] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+	{
+		num = n * -1;
+		str[0] = '-';
+	}
+	
+	while (len > 0)
+	{
+		len--;
+		str[len] = (num % 10) + '0';
+		num = num / 10;
+	}	
+	return (str);
+}
